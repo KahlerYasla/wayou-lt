9ba391ba190e883b535f50804f58a10cd7b7d90d
KahlerYasla <garpayyasla@gmail.com>
2024-04-24 20:22:40 +0300

    Architecture fix

--- backend/RecommenderService/Models/Recommendation.cs
+++
@@ -1,6 +0,0 @@
-namespace RecommenderService.Models;
-
-public class Recommendation
-{
-    public List<string>? Items { get; set; }
-}
--- backend/RecommenderService/Services/Interfaces/IMatchesService.cs
+++
@@ -1,8 +0,0 @@
-using RecommenderService.Models;
-
-namespace RecommenderService.Services.Interfaces;
-
-public interface IMatchesService
-{
-    Task<Recommendation> GetRecommendationsAsync(string kacKisi);
-}
