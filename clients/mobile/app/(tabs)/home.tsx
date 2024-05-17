1f34d521b9214173a4038d4a7ca5f87295371a3a
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2024-05-18 00:23:38 +0300

    all fronts are finished

--- clients/mobile/app/(tabs)/home.tsx
+++ clients/mobile/app/(tabs)/home.tsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
-import { SafeAreaView, ScrollView, View, Text, Button, TouchableOpacity, Image, ImageBackground, TouchableWithoutFeedback } from "react-native";
+import { SafeAreaView, ScrollView, View, TouchableOpacity, Image, TouchableWithoutFeedback } from "react-native";
 import { Stack, useRouter } from "expo-router";
-import { COLORS, FONT, SIZES } from "../../constants";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from '@react-navigation/native';
-import { Drawer } from 'expo-router/drawer';
+import { SIZES } from "../../constants";
 import { Modal } from "react-native";
 import ModalContent from "../../components/HomeScreenModal";
-import CustomButton from "../../components/CustomButton";
+import TinderCard from 'react-tinder-card';
+import cards from "../../constants/Cards"; // Kartların bulunduğu dosya
 
 const Home = () => {
-    const router = useRouter()
+    const router = useRouter();
     const [searchTerm, setSearchTerm] = useState("");
-
     const [isModalVisible, setIsModalVisible] = useState(false);
+    
+    type Direction = 'up' | 'down' | 'left' | 'right';
+    type MyIdentifier = string;
+    const [resetCard, setResetCard] = useState(false);
+
+    // Kart dizisi ve mevcut kart indeksi
+    const cardArray = [cards.card1, cards.card2,cards.card3,cards.card4,cards.card5]; // Gerekli tüm kartları buraya ekleyin
+    const [cardIndex, setCardIndex] = useState(0);
 
     const openModal = () => {
         setIsModalVisible(true);
@@ -21,6 +26,30 @@ const Home = () => {
 
     const closeModal = () => {
         setIsModalVisible(false);
+    };
+
+    const onSwipe = (direction: Direction) => {
+        console.log('You swiped: ' + direction);
+        if (direction === "up") {
+            router.push("CardInfos");
+        } else if (direction === "right" || direction === "left") {
+            // Mevcut kart indeksini artır
+            if (cardIndex < cardArray.length - 1) {
+                
+                if(direction === "right" || direction === "left"){
+                    setCardIndex(cardIndex + 1);
+                    setResetCard(true)
+                }
+            } else {
+                setCardIndex(0); // Döngüsel olarak tekrar başa dön
+                setResetCard(true)
+            }
+        }
+    };
+
+    const handleCardReset = () => {
+        setResetCard(false); // Kartı sıfırla
+        setCardIndex(prevIndex => (prevIndex + 1) % cardArray.length); // Bir sonraki kartı göster
     };
 
     return (
@@ -31,23 +60,29 @@ const Home = () => {
                 }}
             />
             <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ height: '100%' }}>
-                <View
-                    style={{
-                        flex: 1,
-                        padding: SIZES.medium,
-                        paddingRight: 0,
-                    }}>
-                    <View>
-                        <CustomButton textStyle={{color:"white"}} onPress={()=>router.push("CardInfos")} title="go card infos"></CustomButton>
-                    </View>
-                <TouchableOpacity onPress={openModal} style={{top:150,position :"absolute",marginEnd:0, paddingEnd:0, justifyContent:"center",alignItems:"center",backgroundColor:'rgba(19, 16, 20, 0.8)',alignSelf:"flex-end",borderBottomLeftRadius:20,borderTopLeftRadius:20, width:50,height:50,}}>
-                    <View style={{position :"absolute",marginEnd:0, paddingEnd:0, justifyContent:"center",alignItems:"center",backgroundColor:'rgba(19, 16, 20, 0.8)',alignSelf:"flex-end",borderBottomLeftRadius:20,borderTopLeftRadius:20, width:50,height:50,}}>
-                    <Image
-                        style ={{}}
-                            source={require("../../assets/images/burgerMenu.png")}
-                        />
-                    </View>
-                </TouchableOpacity>
+                <View style={{ flex: 1, padding: 0, paddingRight: 0 }}>
+                    <TinderCard 
+                        onSwipe={onSwipe} 
+                        key={resetCard ? "reset" : "notReset"}
+                        onCardLeftScreen={handleCardReset}
+                        preventSwipe={['down']}
+                    >
+                        <View style={{ backgroundColor: 'white', height: "100%", justifyContent: 'center', alignItems: 'center' }}>
+                            <Image
+                                resizeMode="cover"
+                                source={cardArray[cardIndex]}
+                                style={{ width: '100%', height: '100%' }}
+                            />
+                        </View>
+                    </TinderCard>
+                    <TouchableOpacity onPress={openModal} style={{ top: 150, position: "absolute", marginEnd: 0, paddingEnd: 0, justifyContent: "center", alignItems: "center", backgroundColor: 'rgba(19, 16, 20, 0.8)', alignSelf: "flex-end", borderBottomLeftRadius: 20, borderTopLeftRadius: 20, width: 50, height: 50 }}>
+                        <View style={{ position: "absolute", marginEnd: 0, paddingEnd: 0, justifyContent: "center", alignItems: "center", backgroundColor: 'rgba(19, 16, 20, 0.8)', alignSelf: "flex-end", borderBottomLeftRadius: 20, borderTopLeftRadius: 20, width: 50, height: 50 }}>
+                            <Image
+                                style={{}}
+                                source={require("../../assets/images/burgerMenu.png")}
+                            />
+                        </View>
+                    </TouchableOpacity>
                 </View>
             </ScrollView>
             <Modal
---
+++ clients/mobile/constants/Cards.js
@@ -0,0 +1,8 @@
+import card1 from "../assets/images/tinderCards/card1.png";
+import card2 from "../assets/images/tinderCards/card2.png";
+import card3 from "../assets/images/tinderCards/card3.png";
+import card4 from "../assets/images/tinderCards/card4.png";
+import card5 from "../assets/images/tinderCards/card5.png";
+
+
+export default { card1, card2,card3,card4,card5};
